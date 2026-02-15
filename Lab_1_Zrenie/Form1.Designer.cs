3b1265566d2f2d351a6d072a2752ceb7edfd0776
Mi <Mi@DESKTOP-I0LCP8E>
2026-02-15 15:34:29 +0300

    Добавьте файлы проекта.

---
+++ Lab_1_Zrenie/Form1.Designer.cs
@@ -0,0 +1,260 @@
+﻿namespace Lab_1_Zrenie
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle10 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle11 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle12 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnGenerator = new System.Windows.Forms.Button();
+            this.txtNumString = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.X = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Y = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Z = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnDraw = new System.Windows.Forms.Button();
+            this.txtPeriod = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(22, 28);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(308, 73);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Открыть файл";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(433, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(699, 312);
+            this.textBox1.TabIndex = 1;
+            // 
+            // btnGenerator
+            // 
+            this.btnGenerator.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnGenerator.Location = new System.Drawing.Point(22, 349);
+            this.btnGenerator.Name = "btnGenerator";
+            this.btnGenerator.Size = new System.Drawing.Size(308, 124);
+            this.btnGenerator.TabIndex = 2;
+            this.btnGenerator.Text = "Сгенерировать файл";
+            this.btnGenerator.UseVisualStyleBackColor = true;
+            this.btnGenerator.Click += new System.EventHandler(this.btnGenerator_Click);
+            // 
+            // txtNumString
+            // 
+            this.txtNumString.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.txtNumString.Location = new System.Drawing.Point(22, 260);
+            this.txtNumString.Name = "txtNumString";
+            this.txtNumString.Size = new System.Drawing.Size(308, 49);
+            this.txtNumString.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(15, 200);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(331, 42);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Количество строк";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(22, 107);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(308, 73);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Сохранить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.ColumnHeadersHeight = 46;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.X,
+            this.Y,
+            this.Z});
+            this.dataGridView1.Location = new System.Drawing.Point(433, 349);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 82;
+            this.dataGridView1.RowTemplate.Height = 33;
+            this.dataGridView1.Size = new System.Drawing.Size(699, 400);
+            this.dataGridView1.TabIndex = 6;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // X
+            // 
+            dataGridViewCellStyle10.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            this.X.DefaultCellStyle = dataGridViewCellStyle10;
+            this.X.HeaderText = "X";
+            this.X.MinimumWidth = 10;
+            this.X.Name = "X";
+            this.X.Width = 60;
+            // 
+            // Y
+            // 
+            dataGridViewCellStyle11.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            this.Y.DefaultCellStyle = dataGridViewCellStyle11;
+            this.Y.HeaderText = "Y";
+            this.Y.MinimumWidth = 10;
+            this.Y.Name = "Y";
+            this.Y.Width = 60;
+            // 
+            // Z
+            // 
+            dataGridViewCellStyle12.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            this.Z.DefaultCellStyle = dataGridViewCellStyle12;
+            this.Z.HeaderText = "Z";
+            this.Z.MinimumWidth = 10;
+            this.Z.Name = "Z";
+            this.Z.Width = 60;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Black;
+            this.pictureBox1.Location = new System.Drawing.Point(1169, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(400, 400);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnDraw
+            // 
+            this.btnDraw.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnDraw.Location = new System.Drawing.Point(22, 500);
+            this.btnDraw.Name = "btnDraw";
+            this.btnDraw.Size = new System.Drawing.Size(308, 112);
+            this.btnDraw.TabIndex = 8;
+            this.btnDraw.Text = "Отобразить точки";
+            this.btnDraw.UseVisualStyleBackColor = true;
+            this.btnDraw.Click += new System.EventHandler(this.btnDraw_Click);
+            // 
+            // txtPeriod
+            // 
+            this.txtPeriod.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.txtPeriod.Location = new System.Drawing.Point(22, 692);
+            this.txtPeriod.Name = "txtPeriod";
+            this.txtPeriod.Size = new System.Drawing.Size(308, 49);
+            this.txtPeriod.TabIndex = 9;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(22, 642);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(161, 42);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Период ";
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.Location = new System.Drawing.Point(22, 764);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(445, 132);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "Отобразить последовательность";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(2062, 971);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtPeriod);
+            this.Controls.Add(this.btnDraw);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtNumString);
+            this.Controls.Add(this.btnGenerator);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnGenerator;
+        private System.Windows.Forms.TextBox txtNumString;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn X;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Y;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Z;
+        private System.Windows.Forms.Button btnDraw;
+        private System.Windows.Forms.TextBox txtPeriod;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Lab_1_Zrenie/Form1.cs
@@ -0,0 +1,249 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Rebar;
+
+namespace Lab_1_Zrenie
+{
+    public partial class Form1 : Form
+    {
+        Random random = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    textBox1.Text = File.ReadAllText(openFileDialog1.FileName);
+
+                    dataGridView1.Rows.Clear();
+
+                    string[] lines = textBox1.Lines;
+                    foreach (string line in lines)
+                    {
+                        if (string.IsNullOrWhiteSpace(line)) continue;
+
+                        string[] parts = line.Split(new char[] { ' ', ';' }, StringSplitOptions.RemoveEmptyEntries);
+
+                        if (parts.Length >= 3)
+                        {
+                            dataGridView1.Rows.Add(parts[0], parts[1], parts[2]);
+                        }
+                    }
+
+                    MessageBox.Show($"Загружено строк: {dataGridView1.Rows.Count}", "Готово", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Ошибка: {ex.Message}", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        string generator()
+        {
+            string x = random.Next(1024).ToString();
+            string y = random.Next(1024).ToString();
+            string z = random.Next(1024).ToString();
+
+            string stroka = $"{x} {y} {z}";
+            return stroka;
+        }
+
+        private void btnGenerator_Click(object sender, EventArgs e)
+        {
+            using (StreamWriter sw = new StreamWriter("data.txt"))
+            {
+                int numString = int.Parse(txtNumString.Text);
+
+                for (int i = 0; i < numString; i++)
+                {
+                    string s = generator();
+                    sw.WriteLine(s);
+                }
+            }
+            
+        }
+        
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            using (var savefile = new SaveFileDialog())
+            {
+                savefile.FileName = "new_data.csv";
+
+                if (savefile.ShowDialog() == DialogResult.OK)
+                {
+                    try
+                    {
+                        string[] lines = textBox1.Lines;
+                        string[] result = new string[lines.Length];
+
+                        for (int i = 0;i < result.Length;i++)
+                        {
+                            string[] line = lines[i].Split(new char[] {' ', ';'}, StringSplitOptions.RemoveEmptyEntries);
+                            result[i] = string.Join(";", line);
+                        }
+                        File.WriteAllLines(savefile.FileName, result, Encoding.UTF8);
+                        MessageBox.Show("Файл сохранён", "Готово", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show($"Ошибка: {ex.Message}", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+        }
+
+        private void btnDraw_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.Rows.Count == 0) return;
+
+            var bmp = new Bitmap(400, 400);
+            using (var gfx = Graphics.FromImage(bmp))
+                gfx.Clear(Color.Black);
+
+            const double scale = 400.0 / 1024.0;
+
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+            {
+                if (row.IsNewRow) continue;
+                try
+                {
+                    int x = int.Parse(row.Cells[0].Value?.ToString() ?? "0");
+                    int y = int.Parse(row.Cells[1].Value?.ToString() ?? "0");
+                    int z = int.Parse(row.Cells[2].Value?.ToString() ?? "0");
+
+                    int px = (int)(x * scale);
+                    int py = 400 - 1 - (int)(y * scale);
+                    px = Math.Max(0, Math.Min(px, 400 - 1));
+                    py = Math.Max(0, Math.Min(py, 400 - 1));
+
+                    Color color = GetIronColor(z);
+
+                    for (int dy = -1; dy <= 1; dy++)
+                    {
+                        for (int dx = -1; dx<= 1; dx++)
+                        {
+                            int nx = px + dx;
+                            int ny = py + dy;
+                            if (nx >= 0 && nx < 400 && ny >= 0 && ny < 400)
+                            {
+                                bmp.SetPixel(nx, ny, color);
+                            }
+                        }
+                    }
+                }
+
+                catch { }
+            }
+
+            pictureBox1.Image?.Dispose();
+            pictureBox1.Image = bmp;
+        }
+
+        private Color GetIronColor(int z)
+        {
+            double t = z / 1023.0;
+            byte r, g, b;
+
+            if (t < 0.25)
+            {
+                r = (byte)(t * 4 * 128);
+                g = 0;
+                b = 255;
+            }
+
+            else if (t < 0.5)
+            {
+                r = (byte)(128 + (t - 0.25) * 4 * 127);
+                g = 0;
+                b = (byte)(255 - (t - 0.25) * 4 * 255);
+            }
+
+            else if (t < 0.75)
+            {
+                r = 255;
+                g = (byte)((t - 0.5) * 4 * 255);
+                b = 0;
+            }
+
+            else
+            {
+                r = 255;
+                g = 255;
+                b = (byte)((t - 0.75) * 4 * 255);
+            }
+
+            return Color.FromArgb(r, g, b);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int count = dataGridView1.RowCount;
+            int period = int.Parse(txtPeriod.Text);
+            int strok = (count + period - 1)/period;
+
+            int width = period * 10;
+            int hight = strok * 10;
+
+
+            var bmp = new Bitmap(width, hight);
+
+            using (var gfx = Graphics.FromImage(bmp))
+                gfx.Clear(Color.Black);
+            try
+            {
+                for (int i = 0; i < count; i++)
+                {
+                    int x = int.Parse(dataGridView1.Rows[i].Cells[0].Value?.ToString());
+                    Color color = GetIronColor(x);
+
+                    for (int dx = 0; dx < 10; dx++)
+                    {
+                        for (int dy = 0; dy < 10; dy++)
+                        {
+                            bmp.SetPixel((i % period)*10 + dx, (i / period)*10 + dy, color);
+                        }
+                    }
+                }
+            }
+
+            catch { }
+
+            pictureBox1.Image?.Dispose();
+            pictureBox1.Image = bmp;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        
+    }
+}
